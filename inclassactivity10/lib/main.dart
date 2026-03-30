6e7e2a03d001ea2b2f90a6899c5fed06b0f54a82
Bigg PAPA <nito03@icloud.com>
2026-03-30 18:09:26 -0400

    screens folder

--- inclassactivity10/lib/main.dart
+++ inclassactivity10/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'intor.dart';
+import 'screens/intor.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ inclassactivity10/lib/screens/intor.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+class Intor extends StatelessWidget {
+  const Intor({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stages'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SignupPage(),
+                ),
+              );
+            },
+            child: const Text('Go to Signup Page'),
+          ),
+        ],
+      ),
+    );
+  }
+}
